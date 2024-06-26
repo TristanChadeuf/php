9dc1c80d15bfde0811a6c3f1ba5d9ad5800ddc1b
Tristan Chadeuf <tristan.chadeuf@le-campus-numerique.fr>
2024-06-26 16:18:49 +0200

    creation fiche contact

--- contact.php
+++ contact.php
@@ -1,69 +1,104 @@
 
-<?php
-$title = "Contact";
+<?php 
 include 'header.php';
 ?>
+<?php
+session_start();
+?>
 
-<form action="index.php?contenu=submit" method="post">
+<?php
+$_SESSION["Prenom"]=$_POST['Prenom'];
+$_SESSION["Nom"]=$_POST['Nom'];
+$_SESSION["email"]=$_POST['email'];
+$_SESSION["message"]=$_POST['message'];
+
+?>
+
+<?php
+$resultat="recuperation.txt";
+$valeurtableau = array(
+    $civilite= $_POST['civilite'] . "\n",
+    $Prenom= $_POST['Prenom'] . "\n",
+    $Nom= $_POST['Nom']. "\n",
+    $email=$_POST['email']. "\n",
+    $RaisondeContact=$_POST['RaisondeContact']. "\n",
+    $message=$_POST['message']. "\n",
+    );
+file_put_contents($resultat, $valeurtableau, FILE_APPEND);
+
+
+$title = "Contact";
+$erreurprenom = $erreurnom = $erreurcivilite = $erreuremail = $erreurradio = $erreurmessage = "";
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+
+
+if(empty ($_POST['Prenom'])){
+        $erreurprenom= "Prenom manquant !" ;
+}
+if(empty ($_POST['Nom'])){
+        $erreurnom="Nom manquant";
+ }
+ if(empty ($_POST['email'])){
+    $erreuremail="Adresse mail pas valide";
+ }
+ if(empty ($_POST['RaisondeContact'])){
+    $erreurradio="Clickez sur un choix";
+ }
+ if(strlen(($_POST['message']<5))){
+    $erreurmessage="Minimum 5 caracteres";
+ }
+}
+
+?>
+<form action="index.php?contenu=contact" method="post">
 
     
-        <label for="civilité"></label>
-        <select name="genre">
+        <label for="civilite"></label>
+        <select name="civilite" id="civilite">
+        
         <option value="Femme">Femme</option>
         <option value="Homme">Homme</option>
     
+        </select>
+
     <div>
-        <?php
-        if(empty ($_POST['Prenom'])){
-                echo"Remplisser Prénom";
-        }
-        
-        if(empty ($_POST['Nom'])){
-            echo"Remplisser Nom";
-         }
-         if(filter_var ($_POST['email'])){
-            echo"Adresse Mail valide";
-         }else{
-            echo"Adresse Mail invalide";
-         }
-         if(isset ($_POST['RaisondeContact'])){
-            print $_POST['RaisondeContact'];
-         }else{
-            echo"Pas dans les choix valide";
-         }
-         if(strlen(($_POST['message']<=4))){
-            echo"Minimum 5 caractères";
-         }
-    ?>
         <label for="Prenom"></label>
-        <input placeholder=Prenom type="text" name="Prenom">
+        <input placeholder=Prenom type="text" name="Prenom" value = "<?php echo $_SESSION["Prenom"]?>">
+        <span><?php echo $erreurprenom ?></span>
     </div>
 
     <div>
         <label for="Nom"></label>
-        <input placeholder=Nom type="text" name="Nom">
+        <input placeholder=Nom type="text" name="Nom" value="<?php echo $_SESSION["Nom"]?>">
+        <span><?php echo $erreurnom ?></span>
     </div>
 
     <div>
         <label for="email"></label>
-        <input   placeholder=Email type="email" name="email">
+        <input   placeholder=Email type="email" name="email" value="<?php echo $_SESSION["email"]?>">
+        <span><?php echo $erreuremail?></span>
     </div>
 
-    <div>
-        <input for=choix type="radio" name="RaisondeContact" value="achat">
-        <label for="choix">Achat</label>
-    </div>
-    <div>
-        <input for=choix type="radio" name="RaisondeContact" value="vente">
-        <label for="choix">Vente</label>
-    </div>
-    <div>
-        <input for=choix type="radio" name="RaisondeContact" value="Information">
-        <label for="choix">Information</label>
-    </div>
+                    <div>
+                        <input for=choix type="radio" name="RaisondeContact" value="achat">
+                        <label for="choix">Achat</label>
+                        <span><?php echo $erreurradio?></span>
+                    </div>
+                    <div>
+                        <input for=choix type="radio" name="RaisondeContact" value="vente">
+                        <label for="choix">Vente</label>
+                        <span><?php echo $erreurradio?></span>
+                    </div>
+                    <div>
+                        <input for=choix type="radio" name="RaisondeContact">
+                        <label for="choix">Information</label>
+                        <span><?php echo $erreurradio?></span>
+                    </div>
     <div>
         <label for="message"></label>
         <textarea placeholder="Commentaire" name="message"></textarea>
+        <span><?php echo $erreurmessage?></span>
     </div>
 
     <button type="submit">Envoyer</button>
